9947da56fdc3b9c15f1c94805688a19d7dc35b86
tomasdavola <86589591+tomasdavola@users.noreply.github.com>
2023-10-03 21:41:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from rlcard.games.nolimitholdem.round import Action
 import matplotlib.pyplot as plt
 from rlcard.games.nolimitholdem.game import NolimitholdemGame
+
 from toBinary import state_to_binary
+
 env = NolimitholdemGame(allow_step_back=False, num_players=2)
 import tensorflow as tf
 import numpy as np
@@ -19,10 +21,9 @@ class NeuralNetwork(tf.keras.Model):
         x = self.dense1(inputs)
         return self.dense2(x)
 
-# Define the environment (replace with your specific RL environment)
-# For this example, we'll create a random environment
-num_states = 147
-num_actions = 5
+
+num_states = 147 #Length of Binary Inputs
+num_actions = 5 #Output
 
 # Create the neural network model
 model = NeuralNetwork(num_actions)
@@ -30,29 +31,34 @@ model = NeuralNetwork(num_actions)
 # Define the optimizer (e.g., Adam)
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
-# Training loop (for demonstration purposes)
+
 num_episodes = 1000
 for episode in range(num_episodes):
     state = env.init_game()
+    #Converts state to a binary list
     state = state_to_binary(state)
+    #Converts state to a numpy array
     state = np.array(state)
 
     with tf.GradientTape() as tape:
-        for _ in range(10000):  # Adjust the maximum time steps as needed
-            # Replace this with your policy for selecting actions
+        for _ in range(10000):
             action_probs = model(tf.convert_to_tensor(state.reshape(1, -1), dtype=tf.float32))
             action = np.argmax(action_probs)
+            #Returns a number through 0-5
             action = actions[action]
+            #Turns number to an action object
 
-            # Simulate the environment and get the next state and reward
             try:
                 next_state=env.step(action)
                 next_state=np.array(state_to_binary(next_state))
             except:
+                #If this is exception is caught the NN has played an illegal move
                 done=True
                 reward=-1
 
-            # Replace with your environment's next state
+
+
+            #Below is not fully finished/tested
             reward = np.random.randn()
 
             # Calculate the TD error and loss
